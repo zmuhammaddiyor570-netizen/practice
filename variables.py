5c0fa59141fdbd47d35ff4d66818b66e5a06adbd
NEO <zmuhammaddiyor570@gmail.com>
2026-05-28 10:33:04 +0500

    feat : variables lesson

---
+++ variables.py
@@ -0,0 +1,13 @@
+x=20
+y="Ali"
+print(x)
+print(y)
+
+# 2-topshiriq
+x = 4        # x = 4 bo'ldi
+x = "Sally"  # x = "Sally" bo'ldi (4 o'chirildi)
+print(x)     # eng oxirgi qiymat = "Sally"
+x=15
+print(x)
+x="Sally"
+print(x)
